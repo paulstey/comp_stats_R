8d30eb387c8266ae71fe4383fb54b9005e056b19
Paul Stey <paulstey@gmail.com>
2015-08-26 10:14:54 -0400

    cleaning up a bit

--- stey_fisher_cfa.R
+++ stey_fisher_cfa.R
@@ -1,12 +1,8 @@
-####################################################
-# Computational Stats.
-# Homework 9
-# Fisher Scoring for CFA/SEM
+############
+# Fisher scoring for CFA/SEM
 # November 9, 2013
 # Author: Paul Stey
-####################################################
-
-
+############
 
 
 ## Read in Mardia, Kent, and Bibby (1979) "Open-Closed Book" data
@@ -17,58 +13,61 @@ dset <- read.table('http://www3.nd.edu/~kyuan/courses/CS/data/mardia.dat', heade
 
 
 
-###########################################
+#########
 # One-factor model 
 # Using last 3 variables
-###########################################
+#########
 
 X <- dset[, 3:5]
 
 ## initialize arrays to be filled
-F.dot <- rep(NA, length = 6)
-Sigma.dot <- array(NA, dim = c(3, 3, 6))
-F2.dot <- matrix(NA, nrow = 6, ncol = 6)
+f_dot <- rep(NA, length = 6)
+sigma_dot <- array(NA, dim = c(3, 3, 6))
+f_2dot <- matrix(NA, nrow = 6, ncol = 6)
 
 
 ###
-# function to fill F.dot vector (first derivatives)
+# function to fill f_dot vector (first derivatives)
 ###
-dF <- function(X, Sigma, Sigma.dot){
-	## bookkeeping
-	J <- 2*nrow(Sigma)
-	S <- cov(X)
+dx <- function(X, sigma, sigma_dot){
+    
+    J <- 2*nrow(sigma)
+    S <- cov(X)
 
-	## Invert Sigma outside loop
-	Sigma.inv <- solve(Sigma)
+    ## Invert sigma outside loop
+    sigma_inv <- solve(sigma)
 
-	## Fill F.dot with this loop
-	for(j in 1:J){
-		F.dot[j] <- -sum(diag(Sigma.inv %*% (S - Sigma) %*% Sigma.inv %*% Sigma.dot[, , j]))
-	}
-	return(F.dot)
+    ## Fill f_dot with this loop
+    for(j in 1:J){
+        
+        f_dot[j] <- -sum(diag(sigma_inv %*% (S - sigma) %*% sigma_inv %*% sigma_dot[, , j]))
+    }
+    return(f_dot)
 }
 
 
 
 
 ###
-# function to fill F2.dot matrix (second derivates)
+# function to fill f_2dot matrix (second derivates)
 ###
-d2F <- function(X, Sigma, Sigma.dot){
-	## bookkeeping
-	J <- 2*nrow(Sigma)
-	K <- J
+d2x <- function(X, sigma, sigma_dot){
+    
+    J <- 2*nrow(sigma)
+    K <- J
 
-	## Invert Sigma outside loops
-	Sigma.inv <- solve(Sigma)
+    ## Invert sigma outside loops
+    sigma_inv <- solve(sigma)
 
-	## Fill F2.dot with this loop
-	for(j in 1:J){
-		for(k in 1:K){
-			F2.dot[j, k] <- sum(diag(Sigma.inv %*% Sigma.dot[, , j] %*% Sigma.inv %*% Sigma.dot[, , k]))
-		}
-	}
-	return(F2.dot)
+    ## Fill f_2dot with this loop
+    for(j in 1:J){
+        
+        for(k in 1:K){
+            
+            f_2dot[j, k] <- sum(diag(sigma_inv %*% sigma_dot[, , j] %*% sigma_inv %*% sigma_dot[, , k]))
+        }
+    }
+    return(f_2dot)
 }
 
 
@@ -81,43 +80,43 @@ d2F <- function(X, Sigma, Sigma.dot){
 ###
 # Function to return delta theta, which is our step size
 ###
-DelTheta <- function(X, start){
-	## bookkeeping
-	theta <- start
-	J <- length(theta)
-	K <- J
-	lambda <- theta[1:3]
-	
-	## initialize e as identity matrix
-	e <- array(c(1, 0, 0, 0, 1, 0, 0, 0, 1), dim = c(3, 1, 3))
-	
-	## define Psi from theta values
-	Psi <- matrix(c(theta[4], 0, 0, 0, theta[5], 0, 0, 0, theta[6]), nrow = 3, ncol = 3)
+del_theta <- function(X, start){
+    
+    theta <- start
+    J <- length(theta)
+    K <- J
+    lam <- theta[1:3]
+    
+    ## initialize e as identity matrix
+    e <- array(c(1, 0, 0, 0, 1, 0, 0, 0, 1), dim = c(3, 1, 3))
+    
+    ## define psi from theta values
+    psi <- matrix(c(theta[4], 0, 0, 0, theta[5], 0, 0, 0, theta[6]), nrow = 3, ncol = 3)
 
-	## Compute model-implied covariance matrix
-	Sigma <- lambda %*% t(lambda) + Psi
+    ## Compute model-implied covariance matrix
+    sigma <- lam %*% t(lam) + psi
 
 
-	## loop to fill Sigma.dot for j = 1, 2, 3
-	for(i in 1:3){
-		Sigma.dot[, , i] <- e[, , i] %*% t(lambda) + lambda %*% t(e[, , i])
-	}
+    ## loop to fill sigma_dot for j = 1, 2, 3
+    for(i in 1:3){
+        sigma_dot[, , i] <- e[, , i] %*% t(lam) + lam %*% t(e[, , i])
+    }
 
-	## Now, loop to fill Sigma.dot for j = 4, 5, 6
-	for(j in 4:6){
-		Sigma.dot[, , j] <- e[, , j-3] %*% t(e[, , j-3])
-	}
+    ## Now, loop to fill sigma_dot for j = 4, 5, 6
+    for(j in 4:6){
+        sigma_dot[, , j] <- e[, , j-3] %*% t(e[, , j-3])
+    }
 
-	## Apply function for first derivative of F
-	F.dot <- dF(X, Sigma, Sigma.dot)
+    ## Apply function for first derivative of F
+    f_dot <- dx(X, sigma, sigma_dot)
 
-	## Apply function for second derivative of F
-	F2.dot <- d2F(X, Sigma, Sigma.dot)
+    ## Apply function for second derivative of F
+    f_2dot <- d2x(X, sigma, sigma_dot)
 
-	## compute step size 
-	delta.theta <- solve(F2.dot) %*% F.dot
+    ## compute step size 
+    delta_theta <- solve(f_2dot) %*% f_dot
 
-	return(delta.theta)
+    return(delta_theta)
 }
 
 
@@ -127,24 +126,23 @@ DelTheta <- function(X, start){
 # Function that will iterate until sufficiently small step size
 ###
 
-Scoring <- function(X, start, cutoff, max.iter){
-	## bookkeeping
-	theta <- start
-	count <- 1
-	max.step <- 1e5
+scoring <- function(X, start, cutoff, maxiter){
+    
+    theta <- start
+    count <- 1
+    maxstep <- 1e5
 
-	## while loop to minimize step
-	while(max.step > cutoff & count < max.iter){
-		max.step <- max(abs(DelTheta(X, theta)))
-		theta <- theta - DelTheta(X, theta)
+    ## while loop to minimize step
+    while(maxstep > cutoff & count < maxiter){
+        
+        maxstep <- max(abs(del_theta(X, theta)))
+        theta <- theta - del_theta(X, theta)
 
-		## progress monitor
-		print(paste(count, max.step))
-		
-		## update counter
-		count <- count + 1
-	}
-	return(theta)
+        print(paste(count, maxstep))
+        
+        count <- count + 1
+    }
+    return(theta)
 }
 
 
@@ -155,15 +153,15 @@ Scoring <- function(X, start, cutoff, max.iter){
 ###
 theta0 <- c(1, 1, 1, 1, 1, 1)
 
-score.cfa <- Scoring(X, start = theta0, cutoff = 1e-5, max.iter = 100)
+score_cfa <- scoring(X, start = theta0, cutoff = 1e-5, maxiter = 100)
 
 
 ###
 # clean up and print result
 ###
-score.cfa <- round(score.cfa, digits = 2)
+score_cfa <- round(score_cfa, digits = 2)
 
-print(paste0('The factor loadings are: ', score.cfa[1], ', ', score.cfa[2], ', ', 'and ', score.cfa[3], '.  ', 'The unique variances are: ', score.cfa[4], ', ', score.cfa[5], ', ', 'and ', score.cfa[6], '.'))
+print(paste0('The factor loadings are: ', score_cfa[1], ', ', score_cfa[2], ', ', 'and ', score_cfa[3], '.  ', 'The unique variances are: ', score_cfa[4], ', ', score_cfa[5], ', ', 'and ', score_cfa[6], '.'))
 
 
 
@@ -194,10 +192,10 @@ summary(fm1)
 
 
 
-################################################
+##########
 # Two-factor model
 # Using all five variables
-################################################
+##########
 
 
 ## read complete data set into X matrix
@@ -206,29 +204,29 @@ X <- dset
 
 
 ## initialize arrays to be filled
-F.dot <- rep(NA, length = 11)
-Sigma.dot <- array(NA, dim = c(5, 5, 11))
-F2.dot <- matrix(NA, nrow = 11, ncol = 11)
+f_dot <- rep(NA, length = 11)
+sigma_dot <- array(NA, dim = c(5, 5, 11))
+f_2dot <- matrix(NA, nrow = 11, ncol = 11)
 
-## define first derivative of Lambda
-Lambda.dot <- array(0, dim = c(5, 2, 5))
-Lambda.dot[1, 1, 1] <- 1
-Lambda.dot[2, 1, 2] <- 1
-Lambda.dot[3, 2, 3] <- 1
-Lambda.dot[4, 2, 4] <- 1
-Lambda.dot[5, 2, 5] <- 1
+## define first derivative of lam
+lam_dot <- array(0, dim = c(5, 2, 5))
+lam_dot[1, 1, 1] <- 1
+lam_dot[2, 1, 2] <- 1
+lam_dot[3, 2, 3] <- 1
+lam_dot[4, 2, 4] <- 1
+lam_dot[5, 2, 5] <- 1
 
 ## define first derivative of uniquenesses
 e <- array(0, dim = c(5, 5, 5))
 
 for(i in 1:5){
-	e[i, i, i] <- 1
+    e[i, i, i] <- 1
 }
 
 
 
 ## define first derivative of factor covariance matrix
-Phi.dot <- matrix(c(0, 1, 1, 0), nrow = 2, ncol = 2)
+phi_dot <- matrix(c(0, 1, 1, 0), nrow = 2, ncol = 2)
 
 
 
@@ -236,21 +234,21 @@ Phi.dot <- matrix(c(0, 1, 1, 0), nrow = 2, ncol = 2)
 
 
 ###
-# function to fill F.dot vector
+# function to fill f_dot vector
 ###
-dF <- function(X, Sigma, Sigma.dot){
-	## bookkeeping
-	J <- (2 * nrow(Sigma))+ 1
-	S <- cov(X)
+dx <- function(X, sigma, sigma_dot){
+    
+    J <- (2 * nrow(sigma))+ 1
+    S <- cov(X)
 
-	## Invert Sigma outside loop
-	Sigma.inv <- solve(Sigma)
+    ## Invert sigma outside loop
+    sigma_inv <- solve(sigma)
 
-	## Fill F.dot with this loop
-	for(j in 1:J){
-		F.dot[j] <- -sum(diag(Sigma.inv %*% (S - Sigma) %*% Sigma.inv %*% Sigma.dot[, , j]))
-	}
-	return(F.dot)
+    ## Fill f_dot with this loop
+    for(j in 1:J){
+        f_dot[j] <- -sum(diag(sigma_inv %*% (S - sigma) %*% sigma_inv %*% sigma_dot[, , j]))
+    }
+    return(f_dot)
 }
 
 
@@ -258,23 +256,24 @@ dF <- function(X, Sigma, Sigma.dot){
 
 
 ###
-# function to fill F2.dot matrix
+# function to fill f_2dot matrix
 ###
-d2F <- function(X, Sigma, Sigma.dot){
-	## bookkeeping
-	J <- (2 * nrow(Sigma)) + 1
-	K <- J
+d2x <- function(X, sigma, sigma_dot){
+    
+    J <- (2 * nrow(sigma)) + 1
+    K <- J
 
-	## Invert Sigma outside loops
-	Sigma.inv <- solve(Sigma)
+    ## Invert sigma outside loops
+    sigma_inv <- solve(sigma)
 
-	## Fill F2.dot with this loop
-	for(j in 1:J){
-		for(k in 1:K){
-			F2.dot[j, k] <- sum(diag(Sigma.inv %*% Sigma.dot[, , j] %*% Sigma.inv %*% Sigma.dot[, , k]))
-		}
-	}
-	return(F2.dot)
+    ## Fill f_2dot with this loop
+    for(j in 1:J){
+        
+        for(k in 1:K){
+            f_2dot[j, k] <- sum(diag(sigma_inv %*% sigma_dot[, , j] %*% sigma_inv %*% sigma_dot[, , k]))
+        }
+    }
+    return(f_2dot)
 }
 
 
@@ -286,55 +285,54 @@ d2F <- function(X, Sigma, Sigma.dot){
 ###
 # Function to return delta theta, which is our step size
 ###
-DelTheta <- function(X, start){
-	## bookkeeping
-	theta <- start
-	J <- length(theta)
-	K <- J
-	
-	
-	## define Lambda
-	Lambda <- matrix(c(theta[1], theta[2], 0, 0, 0, 0, 0, theta[3], theta[4], theta[5]), nrow = 5, ncol = 2)
+del_theta <- function(X, start){
+    
+    theta <- start
+    J <- length(theta)
+    K <- J
+    
+    
+    ## define lam
+    lam <- matrix(c(theta[1], theta[2], 0, 0, 0, 0, 0, theta[3], theta[4], theta[5]), nrow = 5, ncol = 2)
 
 
-	## define Psi from theta values
-	Psi <- matrix(c(theta[7], 0, 0, 0, 0, 0, theta[8], 0, 0, 0, 0, 0, theta[9], 0, 0, 0, 0, 0, theta[10], 0, 0, 0, 0, 0, theta[11]), nrow = 5, ncol = 5)
+    ## define psi from theta values
+    psi <- matrix(c(theta[7], 0, 0, 0, 0, 0, theta[8], 0, 0, 0, 0, 0, theta[9], 0, 0, 0, 0, 0, theta[10], 0, 0, 0, 0, 0, theta[11]), nrow = 5, ncol = 5)
 
-	## define Phi from theta values
-	Phi <- matrix(c(1, theta[6], theta[6], 1), nrow = 2, ncol = 2)
-
-
-	## Compute model-implied covariance matrix
-	Sigma <- Lambda %*% Phi %*% t(Lambda) + Psi
-	
+    ## define Phi from theta values
+    Phi <- matrix(c(1, theta[6], theta[6], 1), nrow = 2, ncol = 2)
 
 
-	## loop to fill Sigma.dot for j = 1, 2, 3, 4, 5
-	for(j in 1:5){
-		Sigma.dot[, , j] <- Lambda.dot[, , j] %*% Phi %*% t(Lambda) + Lambda %*% Phi %*% t(Lambda.dot[, , j])
-	}
+    ## Compute model-implied covariance matrix
+    sigma <- lam %*% Phi %*% t(lam) + psi
+    
 
 
-	## Compute Sigma.dot for j = 6
-	Sigma.dot[, , 6] <- Lambda %*% Phi.dot %*% t(Lambda)
+    ## loop to fill sigma_dot for j = 1, 2, 3, 4, 5
+    for(j in 1:5){
+        
+        sigma_dot[, , j] <- lam_dot[, , j] %*% Phi %*% t(lam) + lam %*% Phi %*% t(lam_dot[, , j])
+    }
 
 
-	## Loop to fill Sigma.dot for j = 7, 8, 9, 10, 11
-	for(j in 7:11){
-		Sigma.dot[, , j] <- e[, , j-6] %*% t(e[, , j-6])
-	}
+    ## Compute sigma_dot for j = 6
+    sigma_dot[, , 6] <- lam %*% phi_dot %*% t(lam)
 
 
-	## Apply function for first derivative of F
-	F.dot <- dF(X, Sigma, Sigma.dot)
+    ## Loop to fill sigma_dot for j = 7, 8, 9, 10, 11
+    for(j in 7:11){
+        
+        sigma_dot[, , j] <- e[, , j-6] %*% t(e[, , j-6])
+    }
 
-	## Apply function for second derivative of F
-	F2.dot <- d2F(X, Sigma, Sigma.dot)
+    
+    f_dot <- dx(X, sigma, sigma_dot)                        # first deriv. of f
 
-	## compute step size 
-	delta.theta <- solve(F2.dot) %*% F.dot
+    f_2dot <- d2x(X, sigma, sigma_dot)                      # 2nd deriv. of f
 
-	return(delta.theta)
+    delta_theta <- solve(f_2dot) %*% f_dot                  # compute step size
+
+    return(delta_theta)
 }
 
 
@@ -347,24 +345,24 @@ DelTheta <- function(X, start){
 # Function that will iterate until sufficiently small step size
 ###
 
-Scoring <- function(X, start, cutoff, max.iter){
-	## bookkeeping
-	theta <- start
-	count <- 1
-	max.step <- 1e5
+scoring <- function(X, start, cutoff, maxiter){
+    
+    theta <- start
+    count <- 1
+    maxstep <- 1e5
 
-	## while loop to minimize step
-	while(max.step > cutoff & count < max.iter){
-		max.step <- max(abs(DelTheta(X, theta)))
-		theta <- theta - DelTheta(X, theta)
-		
-		## progress monitor
-		print(paste(count, max.step))
+    ## while loop to minimize step
+    while(maxstep > cutoff & count < maxiter){
+        maxstep <- max(abs(del_theta(X, theta)))
+        theta <- theta - del_theta(X, theta)
+        
+        ## progress monitor
+        print(paste(count, maxstep))
 
-		## update counter
-		count <- count + 1
-	}
-	return(theta)
+        ## update counter
+        count <- count + 1
+    }
+    return(theta)
 }
 
 
@@ -375,7 +373,7 @@ Scoring <- function(X, start, cutoff, max.iter){
 ###
 theta0 <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 
-score.cfa <- Scoring(X, start = theta0, cutoff = 1e-5, max.iter = 100)
+score_cfa <- scoring(X, start = theta0, cutoff = 1e-5, maxiter = 100)
 
 
 
@@ -383,9 +381,9 @@ score.cfa <- Scoring(X, start = theta0, cutoff = 1e-5, max.iter = 100)
 ###
 # clean up and print result
 ###
-score.cfa <- round(score.cfa, digits = 2)
+score_cfa <- round(score_cfa, digits = 2)
 
-print(paste0('The factor loadings are: ', score.cfa[1], ', ', score.cfa[2], ', ', score.cfa[3], ', ', score.cfa[4], ', ', 'and ', score.cfa[5], '.  ', 'The unique variances are: ', score.cfa[7], ', ', score.cfa[8], ', ', score.cfa[9], ', ', score.cfa[10], ', ', 'and ', score.cfa[11],'.  ', 'The correlation between factors is ', score.cfa[6], '.'))
+print(paste0('The factor loadings are: ', score_cfa[1], ', ', score_cfa[2], ', ', score_cfa[3], ', ', score_cfa[4], ', ', 'and ', score_cfa[5], '.  ', 'The unique variances are: ', score_cfa[7], ', ', score_cfa[8], ', ', score_cfa[9], ', ', score_cfa[10], ', ', 'and ', score_cfa[11],'.  ', 'The correlation between factors is ', score_cfa[6], '.'))
 
 
 
